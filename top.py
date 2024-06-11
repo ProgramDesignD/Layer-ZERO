cf01d59f3f45d06d1cf0895b8d05bc6fb25b32f7
Maruo.S <raspi-maru2004@outlook.jp>
2024-06-11 10:57:34 +0900

    add icon on top page

--- top.py
+++ top.py
@@ -7,16 +7,13 @@ from panda3d.core import TextNode
 # Add some text
 bk_text = "Layer-ZERO"
 textObject = OnscreenText(text=bk_text, pos=(0,0), scale=0.2,
-                          fg=(1, 0.5, 0.5, 1), align=TextNode.ACenter,
-                          mayChange=1)
-
-# Callback function to set  text
-def setText():
-    pass
+                          fg=(1, 0.5, 0.5, 1), align=TextNode.ACenter)
 
 # Add button
 b = DirectButton(text="START",
-                 scale=.1, command=setText, pos=(0, 0, -0.5))
+                 scale=.1, pos=(0, 0, -0.5))
+b2 = DirectButton(text="Icon",
+                 scale=.1, pos=(1.0, 0, 0.8))
 
 # Run the tutorial
 base.run()
