d60449de412e23cdfb3322b5b39ea8e42c627a69
ShuShioiri <m20055@g.metro-cit.ac.jp>
2024-07-23 11:24:55 +0900

    Add createroom

---
+++ createroom.py
@@ -0,0 +1,124 @@
+from direct.gui.DirectGui import *
+from direct.gui.OnscreenText import OnscreenText
+from panda3d.core import TextNode
+
+class CreateRoom(DirectFrame):
+    def __init__(self, parent=None, on_leave=None, **kw):
+        super().__init__(parent, **kw)
+        self.font = loader.loadFont('./fonts/Genjyuu.ttf')
+        self.leave_btn = DirectButton(
+            parent=self,
+            text="戻る",
+            text_font=self.font,
+            scale=.1,
+            pos=(-1.0, 0, 0.7),
+            command=on_leave
+        )
+
+        self.textObject = OnscreenText(
+            parent=self,
+            text="人数 ",
+            pos=(-0.05, 0.5),
+            scale=0.08,
+            fg=(1, 0.5, 0.5, 1),
+            font=self.font,
+            align=TextNode.ARight,
+            mayChange=1
+        )
+
+        self.players=3
+        self.number = OnscreenText(
+            parent=self,
+            text=str(self.players),
+            pos=(0,0.5),
+            scale=0.15,
+            fg=(1, 0.5, 0.5, 1),
+            align=TextNode.ACenter
+        )
+
+        def inc():
+            global players
+            players_max = 5
+            if self.players >= players_max:
+                return
+            self.players = self.players+1
+            self.number.setText(str(self.players))
+        def dec():
+            global players
+            players_min = 1
+            if self.players <= players_min:
+                return
+            self.players = self.players-1
+            self.number.setText(str(self.players))
+        self.btn_p = DirectButton(
+            parent=self,
+            text=("+1"),
+            command=inc,
+            pos=(0,0,0.65),
+            scale=0.1
+        )
+        self.btn_m = DirectButton(
+            parent=self,
+            text=("-1"),
+            command=dec,
+            pos=(0,0,0.38),
+            scale=0.1
+        )
+
+        self.frm = DirectFrame(
+            parent=self,
+            frameSize=(-0.05,-0.04,-0.05,0.14)
+            ,frameColor=(0,0,0,1)
+            ,pos=(0,0,0.5)
+        )
+        self.frm = DirectFrame(
+            parent=self,
+            frameSize=(-0.05,0.05,0.13,0.14),
+            frameColor=(0,0,0,1),
+            pos=(0,0,0.5)
+        )
+        self.frm = DirectFrame(
+            parent=self,
+            frameSize=(0.04,0.05,-0.05,0.14),
+            frameColor=(0,0,0,1),
+            pos=(0,0,0.5)
+        )
+        self.frm = DirectFrame(
+            parent=self,
+            frameSize=(-0.05,0.05,-0.05,-0.04),
+            frameColor=(0,0,0,1),
+            pos=(0,0,0.5)
+        )
+
+        self.v = [0]
+        self.gamestat = [
+            DirectRadioButton(
+                parent=self,
+                text='公開',
+                text_font=self.font,
+                variable=self.v,
+                value=[0],
+                scale=0.1,
+                pos=(-0.4,0,-0.5)
+            ),
+            DirectRadioButton(
+                parent=self,
+                text='非公開',
+                text_font=self.font,
+                variable=self.v,
+                value=[1],
+                scale=0.1,
+                pos=(0.4,0,-0.5)
+            )
+        ]
+        for button in self.gamestat:
+            button.setOthers(self.gamestat)
+
+        self.confirm_btn = DirectButton(
+            parent=self,
+            text="決定",
+            text_font=self.font,
+            scale=.1,
+            pos=(0, 0, -0.7),
+            # command=confirm
+        )
--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from roommenu import RoomMenu
 from top import TopMenu
 from deckmenu import DeckMenu
 from makedeck import MakeDeck
+from createroom import CreateRoom
+
 class ZeroLayer(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.scenes={
             "topmenu": TopMenu(parent=self.aspect2d, on_start=lambda: self.changeScene("roommenu"), 
                                                      on_deck= lambda: self.changeScene("deckmenu")),
-            "roommenu": RoomMenu(parent=self.aspect2d, on_leave=lambda: self.changeScene("topmenu")),
             "deckmenu": DeckMenu(parent=self.aspect2d, on_leave=lambda: self.changeScene("topmenu"),
                                                        on_make_deck= lambda role: self.changeScene("makedeck")),
-            "makedeck": MakeDeck(parent= self.aspect2d, on_leave=lambda: self.changeScene("deckmenu"))
+            "makedeck": MakeDeck(parent= self.aspect2d, on_leave=lambda: self.changeScene("deckmenu")),
+            "roommenu": RoomMenu(parent=self.aspect2d, on_leave=lambda: self.changeScene("topmenu"),
+                                                       on_create_room=lambda: self.changeScene("createroom")),
+            "createroom": CreateRoom(parent=self.aspect2d, on_leave=lambda: self.changeScene("roommenu"))
         }
         self.changeScene("topmenu")
     def changeScene(self, scene:str):
