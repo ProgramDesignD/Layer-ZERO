7ea2be53bccf36bb031daf5956466d1d1cfb3f82
junfujik <m20121@g.metro-cit.ac.jp>
2024-08-20 22:47:08 +0900

    [ADD] save deck.

--- makedeck.py
+++ makedeck.py
@@ -1,6 +1,10 @@
+from click import command
+from direct.gui.OnscreenText import OnscreenText
 from direct.gui.DirectGui import *
 from panda3d.core import TextNode
+import json
 
+role_list= {"jikkohan", "kyohan", "naitusha", "kebin", "shain"}
 kyotu_name_list= ["移動", "聞き込み"]
 all_koyu_name_list= {"jikkohan":["盗む", "戦闘", "物理的破壊"],
                     "kyohan":["ハッキング(妨害)", "経路把握", "ピッキング"],
@@ -9,11 +13,48 @@ all_koyu_name_list= {"jikkohan":["盗む", "戦闘", "物理的破壊"],
                     "shain":["通報", "警備機能の追加"]}
 item_name_list= ["けむりだま", "ドローン", "透明マント", "はしご"]
 
+decks={}
+
+for i in role_list: #each role's
+    kyotu={}
+    koyu={}
+    item={}
+    any={}
+    for j in kyotu_name_list:
+        any[j]= 0
+    kyotu["kyotu"]= any
+    any={}
+
+    for j in all_koyu_name_list[i]:
+        any[j]= 0
+    koyu= {"koyu":any}
+    any={}
+
+    for j in item_name_list:
+        any[j]= 0
+    item= {"item":any}
+    any={}
+    decks[i]= [kyotu, koyu, item]
+
+with open('tmp.json', 'wt') as f:
+
+    json.dump(decks, f, indent=2)
+
+
+
+
 class MakeDeck(DirectFrame):
     def __init__(self, role:str, parent=None, on_leave=None, **kw):
         super().__init__(parent, **kw)
 
         self.font = loader.loadFont('./fonts/Genjyuu.ttf') # type: ignore
+
+        def effect_card_sum(arg):
+            corrent_card_sum=0
+            for i in range(len(arg)):
+                if(arg[i] >= str(0) and arg[i] <= str(9)):
+                    corrent_card_sum = corrent_card_sum+int(arg[i:])
+                    textObject.setText(str(corrent_card_sum))
 
 # カード枚数の増加減少
         def inc():
@@ -33,43 +74,42 @@ class MakeDeck(DirectFrame):
 
 # 各アクション種別ごとの中身
         j=0
+        contents=[]
         kyotulist= []
         for i in kyotu_name_list:
-            kyotulist.append(DirectButton(parent=self,text_font= self.font, text=(i, "click!", "roll", "disabled"),
-                                        text_scale=0.1, borderWidth=(0.01, 0.01),command=inc,
-                                        relief=2))
-            # self.btn_inc = DirectButton(
-            #     parent=self,
-            #     text=("+1"),
-            #     command=inc,
-            #     # pos=(0,0,0.65),
-            #     scale=0.1)
-            # self.btn_dec = DirectButton(
-            #     parent=self,
-            #     text=("-1"),
-            #     command=dec,
-            #     pos=(0,0,0.38),
-            #     scale=0.1)
-            j=j+1
+            for j in range(21):
+                contents.append(i+str(j))
+            kyotulist.append(DirectOptionMenu(parent=self, text_font= self.font,items=contents, scale=0.1,text="option",command=effect_card_sum,
+                                                highlightColor=(0.65, 0.65, 0.65, 1)))
+            contents=[]
 
 
         koyu_name_list= all_koyu_name_list[role]  # ロールを認識させて固有アクションを選択する
         j=0
+        contents=[]
         koyulist= []
         for i in koyu_name_list:
-            koyulist.append(DirectButton(parent=self,text_font= self.font, text=(i, "click!", "roll", "disabled"),
-                                        text_scale=0.1, borderWidth=(0.01, 0.01),command=inc,
-                                        relief=2))
-            j=j+1
-
+            for j in range(21):
+                contents.append(i+str(j))
+            koyulist.append(DirectOptionMenu(parent=self, text_font= self.font,items=contents, scale=0.1,text="option",command=effect_card_sum,
+                                                highlightColor=(0.65, 0.65, 0.65, 1)))
+            contents=[]
+        
+        
         j=0
+        contents=[]
         itemlist= []
         for i in item_name_list:
-            itemlist.append(DirectButton(parent=self,text_font= self.font, text=(i, "click!", "roll", "disabled"),
-                                        text_scale=0.1, borderWidth=(0.01, 0.01),command=inc,
-                                        relief=2))
-            j=j+1
+            for j in range(21):
+                contents.append(i+str(j))
+            itemlist.append(DirectOptionMenu(parent=self, text_font= self.font,items=contents, scale=0.1,text="option",command=effect_card_sum,
+                                                highlightColor=(0.65, 0.65, 0.65, 1)))
+            contents=[]
 
+
+
+
+# デフォルト機能
         self.leave_btn = DirectButton(parent=self,
                                         text="戻る",
                                         text_font=self.font,
@@ -103,11 +143,11 @@ class MakeDeck(DirectFrame):
 
                                                 frameSize=(0.0, 0.7, -0.05, 0.59),
                                                 frameColor=(0,0,1,0.5),
-                                                pos=(-0.75, 0, 0),
+                                                pos=(-1.2, 0, 0),
                                                 items=kyotulist,
                                                 numItemsVisible=numItemsVisible,
                                                 forceHeight=itemHeight,
-                                                itemFrame_frameSize=(-0.2, 0.2, -0.37, 0.11),
+                                                itemFrame_frameSize=(-0.3, 0.3, -0.37, 0.11),
                                                 itemFrame_pos=(0.35, 0, 0.4))
 
 
@@ -124,11 +164,11 @@ class MakeDeck(DirectFrame):
 
                                             frameSize=(0.0, 0.7, -0.05, 0.59),
                                             frameColor=(1,0,0,0.5),
-                                            pos=(0, 0, 0),
+                                            pos=(-0.35, 0, 0),
                                             items=koyulist,
                                             numItemsVisible=numItemsVisible,
                                             forceHeight=itemHeight,
-                                            itemFrame_frameSize=(-0.2, 0.2, -0.37, 0.11),
+                                            itemFrame_frameSize=(-0.3, 0.3, -0.37, 0.11),
                                             itemFrame_pos=(0.35, 0, 0.4))
 
 
@@ -147,35 +187,15 @@ class MakeDeck(DirectFrame):
 
                                     frameSize=(0.0, 0.7, -0.05, 0.59),
                                     frameColor=(0,1,0,0.5),
-                                    pos=(0.75, 0, 0),
+                                    pos=(0.5, 0, 0),
                                     items=itemlist,
                                     numItemsVisible=numItemsVisible,
                                     forceHeight=itemHeight,
-                                    itemFrame_frameSize=(-0.2, 0.2, -0.37, 0.11),
+                                    itemFrame_frameSize=(-0.3, 0.3, -0.37, 0.11),
                                     itemFrame_pos=(0.35, 0, 0.4))
-        
 
-
-
-# 現在のデッキ枚数
-        self.textObject = OnscreenText(
-            parent=self,
-            text="枚数 ",
-            pos=(-0.05, -0.8),
-            scale=0.08,
-            fg=(1, 0.5, 0.5, 1),
-            font=self.font,
-            align=TextNode.ARight,
-            mayChange=1
-        )
-
-        self.any_cards=0
-        self.number = OnscreenText(
-            parent=self,
-            text=str(self.any_cards),
-            pos=(0,-0.8),
-            scale=0.15,
-            fg=(1, 0.5, 0.5, 1),
-            align=TextNode.ACenter
-        )
-
+        # Add some text
+        output = ""
+        textObject = OnscreenText(text=output, pos=(0.95, -0.95), scale=0.07,
+                                fg=(1, 0.5, 0.5, 1), align=TextNode.ACenter,
+                                mayChange=1)
